fe5ae5ac5dc021ddf127c051e78a7faa98d4b0ab
Russell Brady <russell.brady29@mail.dcu.ie>
2019-09-30 09:21:58 +0100

    commit

--- react_app/src/components/Portfolio.js
+++ react_app/src/components/Portfolio.js
@@ -8,17 +8,18 @@ class Portfolio extends Component {
     if(data){
       
       console.log(data);
-      var projects = data.map(function(projects){
+      var projects = data.map(function(project){
         
+        var sourcestring = require('../resources/github.png')
         return (
-          <div key={projects.name} className="columns portfolio-item">
+          <div key={project.name} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={projects.html_url} title={projects.name}>
-               <img alt={projects.title} src="https://source.unsplash.com/random"/>
+            <a href={project.html_url} title={project.name}>
+               <img alt={project.title} src={sourcestring}/>
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.name}</h5>
-                     <p>{projects.description}</p>
+                 <h5>{project.name}</h5>
+                     <p>{project.description}</p>
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
